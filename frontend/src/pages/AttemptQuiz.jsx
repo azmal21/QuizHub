cde9324302e3a2123de7f702fec887a724f77a99
azmal21 <mahammadazmal21@gmail.com>
2026-04-22 10:36:29 +0530

    Update AttemptQuiz.jsx

--- frontend/src/pages/AttemptQuiz.jsx
+++ frontend/src/pages/AttemptQuiz.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "../styles/AttemptQuiz.css";
 import api from "../api";
-
+  
 const AttemptQuiz = () => {
   const { quizId } = useParams();
   const navigate = useNavigate();
